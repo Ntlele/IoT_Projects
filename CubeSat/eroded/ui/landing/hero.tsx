ff2addf7e82e0df3aaf2c23237f3bc844f9fa2b9
Ntlele <ntleledavid07@gmail.com>
2024-08-14 18:43:38 +0200

    space adjustment

--- CubeSat/eroded/ui/landing/hero.tsx
+++ CubeSat/eroded/ui/landing/hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="flex flex-col md:grid md:grid-cols-[1fr_2fr] items-center justify-start w-full gap-6 md:gap-8">
+    <div className="flex flex-col md:grid md:grid-cols-[1fr_2fr] items-center justify-start w-full gap-6 md:gap-6">
       {/* Left Column */}
-      <div className="flex flex-col py-8 bg-slate-500 items-center justify-center w-full md:w-auto px-4">
-        <div className="border border-black rounded-lg p-6 flex-col flex w-full md:w-[80%] h-96 mb-6">
-          <div className="flex flex-row justify-between mb-4">
+      <div className="flex flex-col py-4 px-4 bg-slate-500 items-center justify-center w-full md:w-auto ">
+        <div className="border border-black rounded-lg p-6 flex-col flex w-full md:w-[80%] h-96 m-4">
+          <div className="flex flex-row justify-between my-4">
             <div>cloud image</div>
             <div>search</div>
           </div>
@@ -17,8 +17,8 @@ function Hero() {
             <h1 className="text-lg">Date and Time</h1>
           </div>
         </div>
-        <div className="border border-black rounded-lg p-6 flex-col flex w-full md:w-[80%] h-96">
-          <div className="flex flex-row justify-between mb-4">
+        <div className="border border-black rounded-lg p-6 flex-col flex w-full md:w-[80%] h-96 m-4">
+          <div className="flex flex-row justify-between m-4">
             <div>cloud image</div>
             <div>search</div>
           </div>
@@ -32,8 +32,8 @@ function Hero() {
       </div>
 
       {/* Right Column */}
-      <div className="flex flex-col py-7 bg-green-500 w-full md:w-auto px-4">
-        <div className="border border-black rounded-lg p-6 mb-6 flex-col flex w-full md:w-[43.75rem] h-96">
+      <div className="flex flex-col py-4 bg-green-500 w-full md:w-auto px-4 items-center justify-center">
+        <div className="border border-black rounded-lg p-6 m-4 flex-col flex w-full md:w-[100%] h-96">
           <div className="flex flex-row justify-between mb-4">
             <div>cloud image</div>
             <div>search</div>
@@ -45,7 +45,7 @@ function Hero() {
             <h1 className="text-lg">Date and Time</h1>
           </div>
         </div>
-        <div className="border border-black rounded-lg p-6 flex-col flex w-full md:w-[43.75rem] h-96">
+        <div className="border  border-black rounded-lg p-6 m-4 flex-col flex w-full md:w-[100%] h-96">
           <div className="flex flex-row justify-between mb-4">
             <div>cloud image</div>
             <div>search</div>
